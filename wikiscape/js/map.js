9b406cd43133a3f9a68f743d4ba52f758f694a9b
void4 <arndtschnabel@freenet.de>
2020-05-10 09:25:48 +0200

    Generate l15, request tiles in new |-separated format

--- wikiscape/js/map.js
+++ wikiscape/js/map.js
@@ -4,7 +4,7 @@ var div = document.getElementById("map");
 
 function getTile(c) {
   //console.log(c)
-  var url = "tiles/{Z}-{X}-{Y}.png"
+  var url = "tiles/{Z}|{X}|{Y}.png"
     .replace("{Z}", c.zoom)
     .replace("{X}", c.column)
     .replace("{Y}", c.row)
@@ -18,14 +18,54 @@ var layer = po.image().url(getTile);
 var map = po.map()
     .container(div.appendChild(po.svg("svg")))
     .zoomRange([0,15])
-    .zoom(1)
+    .zoom(13)//XXX 1
     .add(layer);
+    //.add(textLayer());
           //.tileSize({x: 256, y: 256});
 
 
 map.add(po.interact())
 .add(po.hash());
 
+function textLayer() {
+  var layer = po.layer(load);
+
+  function load(tile, projection) {
+
+    d3.json(`/tilemeta/${tile.zoom}|${tile.column}|${tile.row}`, function(json) {
+
+      console.log(tile, projection)
+      projection = projection(tile).locationPoint;
+
+      // Add an svg:g for each station.
+      var g = d3.select(tile.element = po.svg("g")).selectAll("g")
+          .data(d3.entries(json))
+        .enter().append("svg:g")
+          .attr("transform", transform);
+
+      for (textinfo of json) {
+        // Add a circle.
+        g.append("svg:circle")
+            .attr("r", 4.5);
+
+        // Add a label.
+        g.append("svg:text")
+            .attr("x", 7)
+            .attr("dy", ".31em")
+            .text(function(d) { return d.text; });
+        }
+
+      function transform(d) {
+        d = projection({lon: d.value[0], lat: d.value[1]});
+        return "translate(" + d.x + "," + d.y + ")";
+      }
+
+    });
+
+  }
+
+  return layer;
+}
 
 map.on("move", function() {
   // get the current zoom
